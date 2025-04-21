c40a7c03d1daf92dc1fa9dcd4ec919e255ca425b
Nada A <148198518+Nadayunus@users.noreply.github.com>
2025-04-21 12:32:51 +0530

    Update SecondLarge.java

--- SecondLarge.java
+++ SecondLarge.java
@@ -1,1 +1,27 @@
-
+public class Main{
+    public static void main(String[] args){
+        int[] arr={3,5,6,7,4,9};
+        System.out.println(secondLargest(arr));
+    }
+    public static int secondLargest(int[] arr){
+        int max1=0,max2=0;
+        if(arr[0]>arr[1]){
+            max1=arr[0];
+            max2=arr[1];
+        }
+        else{
+            max2=arr[0];
+            max1=arr[1];
+        }
+        for(int i=2;i<arr.length;i++){
+            if(arr[i]>max1){
+                max2=max1;
+                max1=arr[i];
+            }
+            else if(arr[i]>max2){
+                max2=arr[i];
+            }
+        }
+        return max2;
+    }
+}
